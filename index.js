e798186f6f3f16a3b32932c9bf866f93f6997368
YevaUsatova <yeva.usatova@gmail.com>
2021-05-06 16:26:18 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove('main#main'); 
+const newHeader = document.createElement('H1');
+newHeader.id = 'victory';
+newHeader.innerHTML="<h1>YOUR-NAME is the champion</h1>";
